fcd9cc3e03f53ab2a4d17c4bf5af57b29e000436
frank2638 <1393437238@qq.com>
2019-12-29 19:38:37 +0800

    学习上传

--- study/flask_study.py
+++ study/flask_study.py
@@ -1,12 +1,13 @@
 #coding=utf-8
+import json
 from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def login():
-    data = {
+    data = json.dumps({
         "username":"Mushishi_xu",
         "password":"111111"
-    }
+    })
     return data
 
 if __name__ == "__main__":
