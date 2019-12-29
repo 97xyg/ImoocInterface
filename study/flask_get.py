6960654c0980288bba738fd7a9a54c6e526abaee
frank2638 <1393437238@qq.com>
2019-12-29 19:38:51 +0800

    学习上传

---
+++ study/flask_get.py
@@ -0,0 +1,37 @@
+#coding=utf-8
+import json
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+@app.route("/")
+def Home():
+    data = json.dumps({
+        "username":"Mushishi_xu",
+        "password":"111111"
+    })
+    return data
+
+@app.route('/passport/user/login',methods=['GET'])
+def Login():
+    username = request.args.get("username")
+    password = request.args.get("password")
+    if username and password:
+        data = json.dumps({
+            "username": username,
+            "password": password,
+            "code":"200",
+            "message":"登录成功",
+            "info":"www.baidu.com"
+        })
+    else:
+        data = json.dumps({
+            "message":"请传参数"
+        })
+
+    return data
+
+
+
+#https://www.imooc.com/passport/user/login?username=Mushishi_xu@163.com&password=111111
+if __name__ == "__main__":
+    app.run()
