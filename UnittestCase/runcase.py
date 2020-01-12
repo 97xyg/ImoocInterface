39dfccde635a4d60af4b2015944c591f2a0aa723
frank2638 <1393437238@qq.com>
2020-01-12 14:48:28 +0800

    学习上传

--- UnittestCase/runcase.py
+++ UnittestCase/runcase.py
@@ -1,7 +1,12 @@
 #coding=utf-8
 import sys
-sys.path.append("D:/www/ImoocInterface/")
+import os
+#sys.path.append("D:/www/ImoocInterface/")
 import unittest
+#获取当前目录的绝对路径
+case_path = os.getcwd()+"/UnittestCase/"
+print(case_path)
+'''
 from UnittestCase.test_case01 import TestCase01
 from UnittestCase.test_case02 import TestCase02
 from UnittestCase.test_case03 import TestCase03
@@ -13,3 +18,8 @@ tests = [case_01,case_02,case_03]
 suite.addTests(tests)
 runner = unittest.TextTestRunner()
 runner.run(suite)
+'''
+discover = unittest.defaultTestLoader.discover(case_path)
+runner = unittest.TextTestRunner()
+runner.run(discover)
+#print(discover)
