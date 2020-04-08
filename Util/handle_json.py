edbe92a360091ef6c27a3c9f79def9563269a58a
frank2638 <1393437238@qq.com>
2020-04-08 08:33:20 +0800

    学习

--- Util/handle_json.py
+++ Util/handle_json.py
@@ -11,10 +11,11 @@ def read_json(file_name=None):
         file_path = base_path+"/Config/user_data.json"
     else:
         file_path = base_path+file_name
-    with open(file_path) as f:
+    with open(file_path,encoding='UTF-8') as f:
         data = json.load(f)
     return data
 
 def get_value(key,file_name=None):
     data = read_json(file_name)
-    return data[key]
+    #return data[key]
+    return data.get(key)
--- Util/handle_result.py
+++ Util/handle_result.py
@@ -6,4 +6,23 @@ base_path = os.getcwd()
 sys.path.append(base_path)
 import json
 from Util.handle_json import get_value
-print(get_value("api3/getbanneradvertver2","/Config/code_message.json"))
+#print(get_value("api3/getbanneradvertver2","/Config/code_message.json"))
+
+'''[
+        {"1006":"token error"},
+        {"10001":"用户名错误"},
+        {"10002":"密码错误"}
+    ]'''
+
+def handle_result(url,code):
+    data = get_value(url,"/Config/code_message.json")
+    if data != None:
+        for i in data:
+            message = i.get(code)
+            if message:
+                return message
+    return None
+
+if __name__ == "__main__":
+    print(handle_result("api3/getbanneradvertver2","10002"))
+
