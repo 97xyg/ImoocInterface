9520ddeabc9fc20dc475a3dfe9aac92b69009b83
frank2638 <1393437238@qq.com>
2020-04-14 18:34:38 +0800

    学习

--- Base/base_request.py
+++ Base/base_request.py
@@ -8,20 +8,20 @@ import requests
 import json
 from Util.handle_json import get_value
 from Util.handle_init import handle_ini
-
+proxies={'http':'http://localhost:8888','https':'http://localhost:8888'}
 class BaseRequest:
-    def send_post(self,url,data):
+    def send_post(self,url,data,proxies=proxies,cookie=None):
         #发送post请求
-        res = requests.post(url=url,data=data).text
+        res = requests.post(url=url,data=data,proxies=proxies,cookies=cookie).text
         return res
 
-    def send_get(self,url,data):
+    def send_get(self,url,data,proxies=proxies,cookie=None):
         #发送get请求
-        res = requests.get(url=url,params=data).text
+        res = requests.get(url=url,params=data,proxies=proxies,cookies=cookie).text
         return res
 
     
-    def run_main(self,method,url,data):
+    def run_main(self,method,url,data,proxies=proxies,cookie=None):
         # 执行方法，传递method、url、data参数
         #return get_value(url)
         base_url = handle_ini.get_value('host')
@@ -30,9 +30,9 @@ class BaseRequest:
         #print(url)
 
         if method == 'get':
-            res = self.send_get(url,data)
+            res = self.send_get(url,data,proxies,cookie)
         else:
-            res =self.send_post(url,data)
+            res =self.send_post(url,data,proxies,cookie)
             try:
                 res = json.loads(res)
             except:
--- Run/run_main.py
+++ Run/run_main.py
@@ -7,12 +7,15 @@ sys.path.append(base_path)
 from Util.handle_excel import excel_data
 import json
 from Util.handle_result import handle_result,handle_result_json,get_result_json
+from Util.handle_cookie import write_cookie,get_cookie_value
 from Base.base_request import request
 #['imooc_001', '登陆', 'yes', None, 'login', 'post', '{"username":"111111"}', 'yes', 'message', None]
+proxies={'http':'http://localhost:8888','https':'http://localhost:8888'}
 class RunMain:
     def run_case(self):
         rows = excel_data.get_rows()
         for i in range(rows):
+            cookie = None
             data = excel_data.get_rows_value(i+2)
             is_run = data[2]
             if is_run == 'yes':
@@ -21,7 +24,10 @@ class RunMain:
                 data1 = data[7]
                 expect_method = data[10]
                 expect_result = data[11]
-                res = request.run_main(method,url,data1)
+                cookie_method = data[9]
+                if cookie_method == 'yes':
+                    cookie = get_cookie_value('app')
+                res = request.run_main(method,url,data1,proxies,cookie)
                 #print(res)
                 code = str(res['errorCode'])
                 message = res['errorDesc']
@@ -54,3 +60,4 @@ class RunMain:
 if __name__ == "__main__":
     run =RunMain()
     run.run_case()
+    
