16ec96650d38ce7c5c6cf36e37ce4e49785ed0c8
frank2638 <1393437238@qq.com>
2020-05-23 12:26:37 +0800

    学习上传

--- Run/run_case_ddt.py
+++ Run/run_case_ddt.py
@@ -29,69 +29,96 @@ class TestRunCaseDdt(unittest.TestCase):
             if is_run == 'yes':
                 is_depend = data[3]
                 data1 = json.loads(data[7])
-                if is_depend:
-                    #获取依赖数据
-                    depend_key = data[4]
+                try:
+                    if is_depend:
+                        #获取依赖数据
+                        depend_key = data[4]
+                        
+                        depend_data = get_data(is_depend)
+                        #print(depend_data)
+                        data1[depend_key] = depend_data
+                    method = data[6]
+                    url = data[5]
                     
-                    depend_data = get_data(is_depend)
-                    #print(depend_data)
-                    data1[depend_key] = depend_data
-                method = data[6]
-                url = data[5]
+                    is_header = data[9]
+                    expect_method = data[10]
+                    expect_result = data[11]
                 
-                is_header = data[9]
-                expect_method = data[10]
-                expect_result = data[11]
-               
-                cookie_method = data[8]
-                if cookie_method == 'yes':
-                    cookie = get_cookie_value('app')
-                if cookie_method == 'write':
-                    ''' 必须是获取到cookie'''
-                    get_cookie = {"is_cookie":"app"}
-                if is_header == 'yes':
-                    header = get_header()
-                res = request.run_main(method,url,data1,cookie,get_cookie,header)
-                #print(code)
-                code = str(res['errorCode'])
-                #print(res['errorCode'])
-                message = res['errorDesc']
-                #message+errorcode
+                    cookie_method = data[8]
+                    if cookie_method == 'yes':
+                        cookie = get_cookie_value('app')
+                    if cookie_method == 'write':
+                        ''' 
+                        必须是获取到cookie
+                        '''
+                        get_cookie = {"is_cookie":"app"}
+                    if is_header == 'yes':
+                        header = get_header()
                 
-                if expect_method == 'mec':
-                    config_message = handle_result(url,code)
-                    '''
-                        if message == config_message:
+                    res = request.run_main(method,url,data1,cookie,get_cookie,header)
+                    #print(code)
+                    code = str(res['errorCode'])
+                    #print(res['errorCode'])
+                    message = res['errorDesc']
+                    #message+errorcode
+                    
+                    if expect_method == 'mec':
+                        config_message = handle_result(url,code)
+                        '''
+                            if message == config_message:
+                                excel_data.excel_write_data(i,13,"通过")
+                            else:
+                                excel_data.excel_write_data(i,13,"失败")
+                                excel_data.excel_write_data(i,14,json.dumps(res))
+                        '''
+                        try:
+                            self.assertEqual(message,config_message)
+                            excel_data.excel_write_data(i,13,"通过")
+                        except Exception:
+                            excel_data.excel_write_data(i,13,"失败")
+                            excel_data.excel_write_data(i,14,json.dumps(res))
+                            raise Exception
+
+
+                    if expect_method == 'errorcode':
+                        '''
+                        if expect_result == code:
                             excel_data.excel_write_data(i,13,"通过")
                         else:
                             excel_data.excel_write_data(i,13,"失败")
                             excel_data.excel_write_data(i,14,json.dumps(res))
-                    '''
-                    self.assertEqual(message,config_message)
-                if expect_method == 'errorcode':
-                    '''
-                    if expect_result == code:
-                        excel_data.excel_write_data(i,13,"通过")
-                    else:
-                        excel_data.excel_write_data(i,13,"失败")
-                        excel_data.excel_write_data(i,14,json.dumps(res))
-                    '''
-                    self.assertEqual(expect_result,code)
-                if expect_method == 'json':
-                    if code == 1000:
-                        status_str='sucess'
-                    else:
-                        status_str='error'
-                    expect_result = get_result_json(url,status_str)
-                    result = handle_result_json(res,expect_result)
-                    '''
-                    if result:
-                        excel_data.excel_write_data(i,13,"通过")
-                    else:
-                        excel_data.excel_write_data(i,13,"失败")
-                        excel_data.excel_write_data(i,14,json.dumps(res))
-                    '''
-                    self.assertTrue(result)
+                        '''
+                        try:
+                            self.assertEqual(expect_result,code)
+                            excel_data.excel_write_data(i,13,"通过")
+                        except Exception as e:
+                            excel_data.excel_write_data(i,13,"失败")
+                            excel_data.excel_write_data(i,14,json.dumps(res))
+                            raise e
+                    if expect_method == 'json':
+                        if code == 1000:
+                            status_str='sucess'
+                        else:
+                            status_str='error'
+                        expect_result = get_result_json(url,status_str)
+                        result = handle_result_json(res,expect_result)
+                        '''
+                        if result:
+                            excel_data.excel_write_data(i,13,"通过")
+                        else:
+                            excel_data.excel_write_data(i,13,"失败")
+                            excel_data.excel_write_data(i,14,json.dumps(res))
+                        '''
+                        try:
+                            self.assertTrue(result)
+                            excel_data.excel_write_data(i,13,"通过")
+                        except Exception as e:
+                            excel_data.excel_write_data(i,13,"失败")
+                            excel_data.excel_write_data(i,14,json.dumps(res))
+                            raise e
+                except Exception as e:
+                    excel_data.excel_write_data(i,13,"失败")
+                    raise e
 
 if __name__ == "__main__":
     unittest.main()
